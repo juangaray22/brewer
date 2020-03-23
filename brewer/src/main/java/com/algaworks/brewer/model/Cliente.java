1082a0e491b276e84b2e488e2cc96a1897a3a4c6
Juan Garay <juangaray22@gmail.com>
2020-03-23 23:27:21 +0000

    Commit diario.

---
+++ brewer/src/main/java/com/algaworks/brewer/model/Cliente.java
@@ -0,0 +1,120 @@
+package com.algaworks.brewer.model;
+/**
+ * 
+ * @author JGaray
+ *
+ */
+public class Cliente {
+
+	private String id;
+	private String nome;
+	private String cpf_cnpj;
+	private String email;	
+	private String telefone;
+	private TipoPessoa tipoPessoa;
+	private String logradouro;
+	private String numero;
+	private String complemento;
+	private String cep;
+	
+	public String getCpf_cnpj() {
+		return cpf_cnpj;
+	}
+
+	public void setCpf_cnpj(String cpf_cnpj) {
+		this.cpf_cnpj = cpf_cnpj;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public TipoPessoa getTipoPessoa() {
+		return tipoPessoa;
+	}
+
+	public void setTipoPessoa(TipoPessoa tipoPessoa) {
+		this.tipoPessoa = tipoPessoa;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	private String estado;
+	private String cidade;
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
+}
---
+++ brewer/src/main/java/com/algaworks/brewer/model/TipoPessoa.java
@@ -0,0 +1,5 @@
+package com.algaworks.brewer.model;
+
+public enum TipoPessoa {
+Fisica, Juridica
+}
